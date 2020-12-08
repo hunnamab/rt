a679d30aa67a84c090d4c5033ad32906c6105b47
root <root@host-79-165-96-32.qwerty.ru>
2020-12-08 12:35:16 -0500

    first try to use opencl

--- buffers.c
+++ buffers.c
@@ -12,15 +12,63 @@
 
 #include "rt.h"
 
+const char *KernelSource = "\n" \
+"__kernel void square(                                                  \n" \
+"   __global float* ray_arr,                                            \n" \
+"   __global float* camera_start,                                       \n" \
+"    __global float *viewport,                                          \n" \
+"   const unsigned int count)                                           \n" \
+"{                                                                      \n" \
+"   int i = get_global_id(0);                                           \n" \
+"   int k = i * 3;                                 						\n" \
+"   if(i < count)                                                       \n" \
+"   {                                                                   \n" \
+"       ray_arr[k] = viewport[k] - camera_start[0];                     \n" \
+"       ray_arr[k + 1] = viewport[k + 1] - camera_start[1];             \n" \
+"       ray_arr[k + 2] = viewport[k + 2] - camera_start[2];             \n" \
+"   }                                                                   \n" \
+"}                                                                      \n" \
+"\n";
+
+void	check_funk(float *ray_arr, float *camera_start, float *viewport, int count)
+{
+	int x = 0;
+	int y = 0;
+	int j = 0;
+	int k = 0;
+	while(y < HEI)
+	{
+		while(x < WID)
+		{
+			k = y * (WID * 3) + (x * 3);
+			ray_arr[k] = viewport[k] - camera_start[0];
+			ray_arr[k + 1] = viewport[k + 1] - camera_start[1];
+			ray_arr[k + 2] = viewport[k + 2] - camera_start[2]; 
+			x++;
+		}
+		x = 0;
+		y++;
+	}
+}
+
 void	get_rays_arr(t_scene *scene)
 {
 	int x;
 	int y;
-	int j;
-
+	int j = 0;
+	float *ray_arr;
+	float *camera_start;
+	float *viewport;
+	int k;
+	camera_start = malloc(sizeof(float) * 3);
+	camera_start[0] = scene->camera.position.x;
+	camera_start[1] = scene->camera.position.y;
+	camera_start[2] = scene->camera.position.z;
+	ray_arr = malloc(sizeof(float) * WID * HEI * 3);
+	viewport = malloc(sizeof(float) * 3 * WID * HEI);
 	x = 0;
 	y = 0;
-	while (y < HEI)
+/*  	while (y < HEI)
 	{
 		while (x < WID)
 		{
@@ -29,7 +77,96 @@ void	get_rays_arr(t_scene *scene)
 			scene->ray_buf[j].start.y = scene->camera.position.y;
 			scene->ray_buf[j].start.z = scene->camera.position.z;
 			scene->ray_buf[j].dir = vector_sub(&scene->viewport[j], \
-												&scene->camera.position);
+												&scene->camera.position);			
+			x++;
+		}
+		x = 0;
+		y++;
+	} */
+	while (y < HEI)
+	{
+		while (x < WID)
+		{
+			j = y * WID + x;
+			k = y * (WID * 3) + (x * 3);
+			viewport[k] = scene->viewport[j].x;
+			viewport[k + 1] = scene->viewport[j].y;
+			viewport[k + 2] = scene->viewport[j].z;
+			scene->ray_buf[j].start.x = camera_start[0];
+			scene->ray_buf[j].start.y = camera_start[1];
+			scene->ray_buf[j].start.z = camera_start[2];
+			x++;
+		}
+		x = 0;
+		y++;
+	}
+	//check_funk(ray_arr, camera_start, viewport, (WID * HEI));
+	//STARTING STUFF
+	int err;
+    unsigned int correct;             // number of correct results returned
+    unsigned int count = WID * HEI * 3;
+    size_t global;                      // global domain size for our calculation
+    size_t local;                       // local domain size for our calculation
+	cl_mem kernel_viewport;
+	cl_mem kernel_camera_start;
+	cl_mem kernel_ray_arr;
+    int i;
+    err = 0;
+    err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, 1, &scene->cl_data.device_id, NULL);
+    scene->cl_data.programs = malloc(sizeof(cl_program) * 1);
+    scene->cl_data.kernels = malloc(sizeof(cl_kernel) * 1);
+    scene->cl_data.context = clCreateContext(0, 1, &scene->cl_data.device_id, NULL, NULL, &err);
+    scene->cl_data.commands = clCreateCommandQueue(scene->cl_data.context, scene->cl_data.device_id, 0, &err);
+    scene->cl_data.programs[0] = clCreateProgramWithSource(scene->cl_data.context, 1, (const char **)&KernelSource, NULL, &err);
+    err = clBuildProgram(scene->cl_data.programs[0], 0, NULL, NULL, NULL, NULL);
+    scene->cl_data.kernels[0] = clCreateKernel(scene->cl_data.programs[0], "square", &err);
+    kernel_ray_arr = clCreateBuffer(scene->cl_data.context,  CL_MEM_READ_WRITE,  sizeof(float) * count, NULL, NULL);
+	kernel_viewport = clCreateBuffer(scene->cl_data.context,  CL_MEM_READ_WRITE,  sizeof(float) * count, NULL, NULL);
+	kernel_camera_start = clCreateBuffer(scene->cl_data.context,  CL_MEM_READ_WRITE,  sizeof(float) * 3, NULL, NULL);
+	clEnqueueWriteBuffer(scene->cl_data.commands, kernel_camera_start, CL_TRUE, 0, sizeof(float) * 3, camera_start, 0, NULL, NULL);
+	clEnqueueWriteBuffer(scene->cl_data.commands, kernel_viewport, CL_TRUE, 0, sizeof(float) * count, viewport, 0, NULL, NULL);
+    clEnqueueWriteBuffer(scene->cl_data.commands, kernel_ray_arr, CL_TRUE, 0, sizeof(float) * count, ray_arr, 0, NULL, NULL);
+    err = 0;
+	count = WID * HEI;
+	global = count;
+    err  = clSetKernelArg(scene->cl_data.kernels[0], 0, sizeof(cl_mem), &kernel_ray_arr);
+    err |= clSetKernelArg(scene->cl_data.kernels[0], 1, sizeof(cl_mem), &kernel_camera_start);
+    err |= clSetKernelArg(scene->cl_data.kernels[0], 2, sizeof(cl_mem), &kernel_viewport);
+	err |= clSetKernelArg(scene->cl_data.kernels[0], 3, sizeof(unsigned int), &count);
+
+    // Get the maximum work group size for executing the kernel on the device
+    //
+    err = clGetKernelWorkGroupInfo(scene->cl_data.kernels[0], scene->cl_data.device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(local), &local, NULL);
+	printf("local == max work group size == %ld\n", local);
+    // Execute the kernel over the entire range of our 1d input data set
+    // using the maximum number of work group items for this device
+    //
+    err = clEnqueueNDRangeKernel(scene->cl_data.commands, scene->cl_data.kernels[0], 1, NULL, &global, &local, 0, NULL, NULL);
+    // Wait for the command commands to get serviced before reading back results
+    //
+    clFinish(scene->cl_data.commands);
+    // Read back the results from the device to verify the output
+    //
+    err = clEnqueueReadBuffer(scene->cl_data.commands, kernel_ray_arr, CL_TRUE, 0, sizeof(float) * count, ray_arr, 0, NULL, NULL );   
+    // Shutdown and cleanup
+    //
+    clReleaseMemObject(kernel_ray_arr);
+    clReleaseProgram(scene->cl_data.programs[0]);
+    clReleaseKernel(scene->cl_data.kernels[0]);
+    clReleaseCommandQueue(scene->cl_data.commands);
+    clReleaseContext(scene->cl_data.context);
+	//END STUFF
+ 	x = 0;
+	y = 0;
+	while (y < HEI)
+	{
+		while (x < WID)
+		{
+			j = y * WID + x;
+			k = y * (WID * 3) + (x * 3);
+			scene->ray_buf[j].dir.x = ray_arr[k];
+			scene->ray_buf[j].dir.y = ray_arr[k + 1];
+			scene->ray_buf[j].dir.z = ray_arr[k + 2];
 			x++;
 		}
 		x = 0;
