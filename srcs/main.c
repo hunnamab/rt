b9f4249e944d6036119f2d3e66dff591765b9686
Peabody Metron <pmetron@il-g5.msk.21-school.ru>
2021-01-27 22:32:14 +0300

    wtf filter

--- srcs/main.c
+++ srcs/main.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldeirdre <ldeirdre@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pmetron <pmetron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 15:39:02 by hunnamab          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/01/27 22:25:43 by pmetron          ###   ########.fr       */
-=======
-/*   Updated: 2021/01/27 22:12:43 by ldeirdre         ###   ########.fr       */
->>>>>>> a068450f2241b9f677ad11e79a212e93959a439a
+/*   Updated: 2021/01/27 22:32:05 by pmetron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -161,7 +157,7 @@ int		main(int args, char **argv)
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_CreateWindowAndRenderer(WID + 350, HEI, 0, &sdl.win, &sdl.renderer);
 	SDL_RenderClear(sdl.renderer);
-	scene->filter_type = SHARPEN;
+	scene->filter_type = DEFAULT;
 	//scene->obj_nmb += 1;
 	//scene->objs[scene->obj_nmb - 1] = new_box(get_point(1,1,1), get_point(10,10,10), set_color(0,0,255,255), get_point(0,0,0), 100);
 	//scene->objs[scene->obj_nmb - 1] = new_paraboloid(get_point(0,0,5), 50, set_color(255,0,0,255), get_point(0,0,0), 100);
