57063cc1cf2f084722977e54a63585374a8412f6
Peabody Metron <pmetron@il-d1.msk.21-school.ru>
2021-02-21 20:46:45 +0300

    norme

--- includes/types.h
+++ includes/types.h
@@ -6,7 +6,7 @@
 /*   By: pmetron <pmetron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 16:33:03 by hunnamab          #+#    #+#             */
-/*   Updated: 2021/02/20 18:32:46 by pmetron          ###   ########.fr       */
+/*   Updated: 2021/02/21 20:03:44 by pmetron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -353,6 +353,7 @@ struct					s_scene
 	t_texture			**texts;
 	t_color				*frame_buf;
 	t_cutting_surface	*srfs;
+	cl_int2 			*shift_buf;
 	int					init_flag;
 	int					srf_nmb;
 	int					box_nmb;
